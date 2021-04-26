8e77753a845beb59d758e195ec33804dc5604116
Archana <archanaelisala@gmail.com>
2021-04-26 16:37:50 +0530

    three files added to repository

---
+++ 02-print.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Two commands are widely used for printing a custom message through script.
+# 1. printf
+# 2. echo
+
+# Printf needs a lot of syntaxes and echo avoids them, Yet both does the same job.
+# Hence we go with echo command
+
+# echo INPUT
+
+echo Hello World
+echo Welcome to DevOps Training
+
+# To print multi line outputs we need to enable escape sequences \n
+# Syntax: echo -e "Hello\nWorld"
+# when we use -e option then give input in quotes, Preferably double quotes
+
+echo -e "Hello\nWorld"
+echo -e "Hello\n\nWorld"
+
+# Color based printing
+
+# Colors      Code
+
+# Red         31
+# Green       32
+# Yellow      33
+# Blue        34
+# Magenta     35
+# Cyan        36
+
+# Reset       0
+# Bold        1
+
+# https://misc.flogisoft.com/bash/tip_colors_and_formatting
+
+# Syntax: echo -e  "\e[COLCODEmMESSAGE"
+
+echo -e "\e[31mHello in Red\e[0m"
+echo -e "\e[1;31mHello in Bold Red\e[0m"
+
+echo  "++++++++++++++  We are done with Printing Options ++++++++++++++++++"
---
+++ 03-variable.sh
@@ -0,0 +1,48 @@
+
+
+#!/bin/bash
+
+# Variable is name you give to certain set of data.
+
+## With out variables
+echo Welcome to DevOps training
+echo Training of DevOps lasts for 70 days
+echo "DevOps training covers shell scripting & Ansible"
+
+## Declaring a variable , Syntax: var=data
+
+COURSE=LINUX
+echo Welcome to ${COURSE} training
+echo Training of $COURSE lasts for 70 days
+echo "$COURSE training covers shell scripting & Ansible"
+
+## Variables usually holds a data which varies from time to time.
+
+## Accessing variable can be done with $var and also ${var}
+
+a=100
+fruit=Banana
+echo $fruit is $ano
+echo $fruit is ${a}no
+
+
+## Variable Names.
+
+# Dynamic Variables
+
+echo Good Morning , Today date is 2021-03-24
+
+# Dynamic variables can be created in two ways
+# 1 . Command Substitution, Declaring a varibale from command output  , Syntax: var=$(commands)
+# 2 . Arithmetic Substitution, Declaring va variable from arthmetic expression, Syntax: var=$((expressions))
+
+DATE=$(date +%F)
+echo Good Morning , Today date is ${DATE}
+echo Good Morning , Today date is $(date +%F)
+
+ADD=$((2+3))
+
+echo Added value of 2+3 = ${ADD}
+
+echo "Env Variable UID = ${UID}"
+echo "ENV variable COURSENAME = ${COURSENAME}"
